9807bac30902b795c563741f01bd54d735b96f7c
Siddhesh Chaughule <siddhesh.chaughule21@gmail.com>
2021-06-06 13:44:12 +0530

    Order module started

--- src/components/BookTickets.js
+++ src/components/BookTickets.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieDataById } from "./moviesCrud";
+import "./booking.scss";
 
 const BookTickets = () => {
   let { id } = useParams();
@@ -9,16 +10,54 @@ const BookTickets = () => {
 
   const fetchMovieData = async () => {
     const movData = await getMovieDataById(id);
-    console.log(movData);
+    setMovieData(movData);
   };
 
   useEffect(() => {
     fetchMovieData();
   }, []);
 
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 600);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 600);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <>
-      <div></div>
+      <div className="booking-main">
+        <div className="booking-box">
+          <div
+            style={
+              isDesktop
+                ? {
+                    display: "flex",
+                    borderRadius: "10px",
+                    backgroundColor: "transparent",
+                  }
+                : {}
+            }
+          >
+            {isDesktop ? (
+              <img
+                src={movieData?.posterImg}
+                alt="Movie theatre image"
+                width="50%"
+                style={{
+                  height: `calc(100vh - 135px)`,
+                  border: "round",
+                  borderRadius: "10px",
+                }}
+              />
+            ) : null}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
